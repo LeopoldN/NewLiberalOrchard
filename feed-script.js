97d2671de9a3b2ba58842244c4650372cb209836
Nick Leopold <nleopold20@gmail.com>
2024-08-19 14:10:45 +0200

    updated periods before slashes

--- feed-script.js
+++ feed-script.js
@@ -125,7 +125,7 @@ function updatePaginationControls() {
 
 // Fetch the JSON data
 let articles = [];
-fetch('articles.json')
+fetch('./articles.json')
     .then(response => response.json())
     .then(data => {
         articles = data.articles;
